c964929864981b1269cdb35040d81b46fb66cea2
Mitch McCullough <mitchmccullough@gmail.com>
2022-10-10 19:18:13 -0500

    added greeting

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+animals = [ "Triceratops", "Gorilla", "Corgi", "Toucan"]
+
+for animal in animals:
+    if len(animal) > 5:
+      print(animal)
+
+greetings = ['howdy', 'whaddup wid it', 'hallo']
+select_greeting = input(f'select greeting from list: {greetings}: ')
+print(select_greeting)
